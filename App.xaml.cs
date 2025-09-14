08eb872e0d495c1fe7c50220eeb79060fca0f772
jeanlucdupont <jl.dupont@gmail.com>
2025-09-14 10:01:11 -0500

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,6 @@
+﻿using System.Windows;
+
+namespace Manekineko
+{
+    public partial class App : Application { }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace Manekineko
+{
+    public partial class MainWindow : Window
+    {
+        private bool _clickThrough = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Start the paw animation
+            if (FindResource("PawWave") is Storyboard sb)
+                sb.Begin();
+
+            // Slight initial transparency for a softer look
+            this.Opacity = 0.98;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Drag the window (anywhere)
+            try { DragMove(); } catch { /* ignore if mouse up */ }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        private void ClickThrough_Click(object sender, RoutedEventArgs e)
+        {
+            _clickThrough = !_clickThrough;
+            SetClickThrough(_clickThrough);
+        }
+
+        #region Click-through (WS_EX_TRANSPARENT) P/Invoke
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter,
+            int X, int Y, int cx, int cy, uint uFlags);
+
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_TRANSPARENT = 0x00000020;
+        const int WS_EX_LAYERED = 0x00080000;
+        const int WS_EX_TOOLWINDOW = 0x00000080; // optional: keeps it off Alt+Tab
+        const uint SWP_NOSIZE = 0x0001;
+        const uint SWP_NOMOVE = 0x0002;
+        const uint SWP_NOZORDER = 0x0004;
+        const uint SWP_FRAMECHANGED = 0x0020;
+
+        private void SetClickThrough(bool enable)
+        {
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            int exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+
+            if (enable)
+            {
+                exStyle |= WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW;
+            }
+            else
+            {
+                // remove transparent flag, keep layered for smoothness; toolwindow optional
+                exStyle &= ~WS_EX_TRANSPARENT;
+            }
+
+            SetWindowLong(hwnd, GWL_EXSTYLE, exStyle);
+            SetWindowPos(hwnd, IntPtr.Zero, 0, 0, 0, 0,
+                SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+        }
+
+        #endregion
+    }
+}
